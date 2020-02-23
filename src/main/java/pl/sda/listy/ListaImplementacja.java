86adb081ca9beb926e8a55c7ac420179ff2cc3d1
lukaszog <lukasz.ogan@gmail.com>
2020-02-23 12:26:38 +0100

    implementacja

--- src/main/java/pl/sda/listy/ListaImplementacja.java
+++ src/main/java/pl/sda/listy/ListaImplementacja.java
@@ -1,10 +1,12 @@
 package pl.sda.listy;
 
+import java.util.Arrays;
+
 public class ListaImplementacja implements Lista {
 
     private int[] liczby;
     private int pojemnosc = 10;
-    private int rozmiar;
+    private int rozmiar = 0;
 
     ListaImplementacja(){
         this.liczby = new int[this.pojemnosc];
@@ -16,8 +18,15 @@ public class ListaImplementacja implements Lista {
 
     @Override
     public void dodajElement(int liczba) {
-
+        if(this.rozmiar == this.pojemnosc){
+            System.out.println("PELNA TABLICA");
+            this.liczby = Arrays.copyOf(this.liczby, this.liczby.length*2);
+            this.pojemnosc = this.liczby.length;
+        }
+        this.liczby[this.rozmiar] = liczba;
+        this.rozmiar++;
     }
+
 
     @Override
     public int znajdz(int liczba) {
@@ -43,4 +52,10 @@ public class ListaImplementacja implements Lista {
     public void zapiszDoPliku(String plik) {
 
     }
+
+    @Override
+    public String toString(){
+        return Arrays.toString(this.liczby);
+    }
+
 }
--- src/main/java/pl/sda/listy/Main.java
+++ src/main/java/pl/sda/listy/Main.java
@@ -6,7 +6,30 @@ public class Main {
 
     public static void main(String[] args) {
         Lista lista = new ListaImplementacja();
-        lista.dodajElement(212);
+        lista.dodajElement(1);
+        lista.dodajElement(2);
+        lista.dodajElement(3);
+        lista.dodajElement(4);
+        lista.dodajElement(5);
+        lista.dodajElement(6);
+        lista.dodajElement(7);
+        lista.dodajElement(8);
+        lista.dodajElement(9);
+        lista.dodajElement(10);
+        lista.dodajElement(11);
+        lista.dodajElement(12);
+        lista.dodajElement(13);
+        lista.dodajElement(14);
+        lista.dodajElement(15);
+        lista.dodajElement(16);
+
+        System.out.println(lista);
+
+        StringBuilder s = new StringBuilder();
+        s.append("Hej");
+        s.append("CZESC");
+        s.append("2221");
+        System.out.println(s);
 
     }
 }
